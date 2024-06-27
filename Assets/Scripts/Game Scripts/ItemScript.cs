9ccc7c38e1c7ae01f4cd2eef117a373c97874770
mushuacan <mushuacan@gmail.com>
2024-06-27 16:26:09 +0300

    Yapımı kolaylaştıracak ögeler düzenlendi
    DOTween eklendi
    Elma modeli değiştirildi
    Dosyaların isimleri güncellendi ve toplandı

--- Assets/Scripts/Game Scripts/ItemScript.cs
+++ Assets/Scripts/Game Scripts/ItemScript.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using System.Collections;
+using DG.Tweening;
+using Unity.VisualScripting;
 
 public class ItemScript : MonoBehaviour
 {
-    public float transitionDuration = 5f; // Rengin dönüþüm süresi
-    public Color targetColor = Color.green; // Hedef renk
-    public Color targetColor2 = Color.red; // Hedef renk
-    public float fadeOutDuration = 1f; // Soluklaþma süresi
-
+    public float colorChangeTime1 = 3f; // Rengin dönüþüm süresi
+    public float colorChangeTime2 = 5f; // Soluklaþma süresi
+    public float colorChangeTime3 = 2f; // Soluklaþma süresi
+    public float lifeTime = 5;
+    public Color targetColor1 = new Color(0.0f, 0.5f, 0.0f); // Koyu yeþil
+    public Color targetColor2 = Color.black; // Siyah
+    public Color targetColor3 = Color.black; // Siyah
+    private Tween colorTween;
     public Renderer rend;
-    private Color originalColor;
 
     public GameObject particleEffectPrefab; // Particle effect prefab'i bu deðiþkene atayýn
 
@@ -20,34 +24,68 @@ public class ItemScript : MonoBehaviour
     {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource = GetComponent<AudioSource>();
-        originalColor = rend.material.color;
-        StartCoroutine(ChangeColorOverTime());
+        rend.material.shader = Shader.Find("Standard"); // Standard shader kullanýldýðýndan emin ol
+        rend.material = new Material(rend.material); // Orijinal materyalin bir kopyasýný oluþtur
+        StartColorTransition();
     }
 
-    IEnumerator ChangeColorOverTime()
+    void StartColorTransition()
     {
-        yield return new WaitForSeconds(transitionDuration);
-
-        float elapsedTime = 0f;
-        while (elapsedTime < fadeOutDuration)
+        // Sararmasý
+        colorTween = rend.material.DOBlendableColor(targetColor1, (lifeTime/10) * colorChangeTime1).OnComplete(() =>
         {
-            float t = elapsedTime / fadeOutDuration;
-            rend.material.color = Color.Lerp(originalColor, targetColor, t);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
+            if (rend != null)
+            {
+                // Yaþarmasý
+                rend.material.DOBlendableColor(targetColor2, (lifeTime / 10) * colorChangeTime2).OnComplete(() =>
+                {
+                    if (rend != null)
+                    {
+                        //Kararmasý
+                        rend.material.DOBlendableColor(targetColor3, (lifeTime / 10) * colorChangeTime3).OnComplete(() =>
+                        {
+                            if (rend != null)
+                            {
+                                ItemNotTaken_Destroy();
+                                // 3 saniye bekleyip kendini imha et
+                                DOVirtual.DelayedCall(3f, () =>
+                                {
+                                    Destroy(gameObject);
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    void ItemNotTaken_Destroy()
+    {
+        // Particle effect oluþtur
         if (particleEffectPrefab != null)
         {
             Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
         }
+
+        // Objeyi aþaðý taþý
         transform.position = new Vector3(transform.position.x, transform.position.y - 4, transform.position.z);
+
+        // Ses çal
         PlaySound();
-        yield return new WaitForSeconds(3f);
-        Destroy(gameObject); // Kendini imha et
     }
+
     private void PlaySound()
     {
         int randomIndex = Random.Range(0, sounds.Length);
         audioSource.PlayOneShot(sounds[randomIndex]);
+    }
+    void OnDestroy()
+    {
+        // Tween'leri objeyi yok etmeden önce durdur
+        if (colorTween != null)
+        {
+            colorTween.Kill();
+        }
     }
 }
